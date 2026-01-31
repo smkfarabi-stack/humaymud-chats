9ee30d240caea329a3dd517861d57068d09a1b91
smk Farabi <smkfarabi@gmail.com>
2026-01-31 17:15:57 +0600

    sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,34 @@
-// A simple Service Worker to make the app installable
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('humaymud-store').then((cache) => cache.addAll([
-      './',
-      './index.html',
-      './manifest.json',
-      './logo.png'
-    ]))
+const CACHE_NAME = 'humaymud-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  'https://unpkg.com/peerjs@1.5.2/dist/peerjs.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+// Activate & Cleanup Old Caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
+    })
+  );
+});
+
+// Fetch Strategy: Network First, Fallback to Cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
